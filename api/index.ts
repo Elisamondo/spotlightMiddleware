f2a7139586c18e732c03f2dffca50d28ca576ebe
Elisa Edson <elisaedson@Elisas-MacBook-Pro.local>
2024-11-28 13:59:10 +0000

    minor modification

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", (req, res) => {
 
   console.log(req)
-  res.send("gotcha")
+  res.send(req)
 })
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
