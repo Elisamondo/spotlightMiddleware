90004bc2ca79f7c22ae5fc511c9f6c15300cdbda
Elisa Edson <elisaedson@Elisas-MacBook-Pro.local>
2024-11-28 13:58:08 +0000

    more stuff

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,36 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+
+  console.log(req)
+  res.send("gotcha")
+})
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
+
+const { Pool } = require('pg');
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  }
+});
+
+async function getPgVersion() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query('SELECT * FROM bags');
+    console.log(result.rows[0]);
+  } finally {
+    client.release();
+  }
+}
