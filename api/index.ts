5e2a2d1c7feae09d4c61d4aba892543ba833912e
Elisa Edson <elisaedson@Elisas-MacBook-Pro.local>
2024-11-28 18:04:00 +0000

    maybe functional

--- api/index.ts
+++ api/index.ts
@@ -7,18 +7,10 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get("/", (req, res) => {
-
-  console.log(req)
-
-  console.log(`req query - ${JSON.stringify(req.query)}`)
-
-  res.send("hiiiii")
-}
-)
-
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
+
+
 
 module.exports = app;
 
@@ -32,3 +24,34 @@ const pool = new Pool({
     require: true,
   }
 });
+
+const client =  pool.connect()
+
+
+app.get("/", (req, res) => {
+
+  console.log(req)
+
+  if (!req.query.SQL){
+    return res.status(400).send({
+      message: 'Invalid SQL Query'
+    })
+  }
+
+
+  let command = req.query.SQL
+
+
+
+  res.send(getQuery(command))
+}
+)
+
+async function getQuery(command){
+  try{
+    const result = await client.query(`${command}`);
+    return result
+  } catch (e){
+    return e
+  }
+}
