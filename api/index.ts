7ba0980bb0a13351f092ad5e56b0c34a2263b943
Elisa Edson <elisaedson@Elisas-MacBook-Pro.local>
2024-12-02 21:43:33 +0000

    table specification

--- api/index.ts
+++ api/index.ts
@@ -75,27 +75,27 @@ app.post("/POST", async (req, res) => {
 
   console.log(`POST Received - \n ${JSON.stringify(util.inspect(req.body))}`)
 
-  let columns = "(notes, origin, dateFound, status, "
+  let columns = `(${req.body.category}.notes, ${req.body.category}.origin, ${req.body.category}.dateFound, ${req.body.category}.status, `
   let values = `VALUES ( "${req.body.notes}", "${req.body.origin}", "${req.body.dateFound}", "${req.body.status}", `
 
   switch (req.body.category){
     case "clothing":
-      columns += "make, type, colours"
+      columns += `${req.body.category}.make, ${req.body.category}.type, ${req.body.category}.colours`
       values += `"${req.body.make}", "${req.body.type}", ${req.body.colours}`
     break
 
     case "bags":
     case "booksPapers":
-      columns += "make, contents"
+      columns += `${req.body.category}.make, ${req.body.category}.contents`
       values += `"${req.body.make}", "${req.body.contents}"`
     break
     case "jewelleryAccessories":
     case "phones":
-      columns += "make, colours"
+      columns += `${req.body.category}.make, ${req.body.category}.colours`
       values += `"${req.body.make}", "${req.body.colours}"`
       break
     case "keychainsWallets":
-      columns += "contents"
+      columns += `${req.body.category}.contents`
       values += `"${req.body.contents}"`
       break
   }
